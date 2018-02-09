d91c5eaaf5c1a793249d6eaff23311d312a2a7cb
wangjiangchuan <wangjiangchuan@pset.suntec.net>
2018-02-09 17:43:55 +0800

    add coroutine

---
+++ code/Note/coroutine_.py
@@ -0,0 +1,22 @@
+
+#协程，使用generator实现
+def consumer():
+    r = ''
+    while True:
+        n = yield r
+        if not n:
+            return
+        print('[CONSUMER] consumed: {0}'.format(n))
+        r = '200 OK'
+
+def procducer(c):
+    c.send(None)
+    n = 0
+    while n < 5:
+        n += 1
+        r = c.send(n)
+        print('[PRODUCER] consumer returned: {0}'.format(r))
+    c.close()
+
+c = consumer()
+procducer(c)
