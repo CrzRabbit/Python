d3c416e4fc3db8fecd3942e20f866a4d2d62f4b0
CrzRabbit <33614042+CrzRabbit@users.noreply.github.com>
2018-03-18 09:39:08 +0800

    fix mergesort

--- methods/mergesort.py
+++ methods/mergesort.py
@@ -1,37 +1,37 @@
 
-def mergesort(list, l, h):
+def mergesort(list):
+    l = 0
+    h = list.__len__()
     if l == h - 1:
-        return
+        return list
     if l == h - 2:
         if list[l] > list[list.__len__() - 1]:
             list[l] = list[l] ^ list[list.__len__() - 1]
             list[list.__len__() - 1] = list[l] ^ list[list.__len__() - 1]
             list[l] = list[l] ^ list[list.__len__() - 1]
-        return
+        return list
 
     listl = list[l: int((l + h) / 2)]
     listr = list[int((l + h) / 2): h]
     list = []
 
-    mergesort(listl, 0, listl.__len__())
-    mergesort(listr, 0, listr.__len__())
-
-    print(listl, listr)
+    listl = mergesort(listl)
+    listr = mergesort(listr)
 
     m = 0
     n = 0
     for i in range(l, h):
         if (n >= listr.__len__()) or ((m < listl.__len__()) and (listl[m] <= listr[n])):
             list.append(listl[m])
-            print(listl[m])
             m += 1
             continue
         if (m >= listl.__len__()) or ((n < listr.__len__()) and (listr[n] <= listl[m])):
             list.append(listr[n])
-            print(listr[n])
             n += 1
             continue
+    print(list)
+    return list
 
 list = [25, 5, 23 , 99, 85, 5, 8]
-mergesort(list, 0, list.__len__())
+list = mergesort(list)
 print(list)
