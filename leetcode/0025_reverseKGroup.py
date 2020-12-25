c9ecfd322d331caa8e962f85b5450da023db46c0
wangjiangchuan <1016864609@qq.com>
2020-12-25 10:01:05 +0800

    add 25 26

---
+++ leetcode/0025_reverseKGroup.py
@@ -0,0 +1,43 @@
+from leetcode.tools.list import *
+from leetcode.tools.time import *
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        ret = ListNode()
+        ret.next = head
+        part = []
+        left = ret
+        right = None
+        temp = head
+        while temp != None and len(part) < k:
+            part.append(temp)
+            temp = temp.next
+        right = temp
+        if len(part) == k:
+            for i in range(1, len(part)):
+                part[i].next = part[i - 1]
+            left.next = part[len(part) - 1]
+            part[0].next = right
+            left = part[0]
+            temp = right
+        while len(part) == k:
+            part.clear()
+            while temp != None and len(part) < k:
+                part.append(temp)
+                temp = temp.next
+            right = temp
+            if len(part) == k:
+                for i in range(1, len(part)):
+                    part[i].next = part[i - 1]
+                left.next = part[len(part) - 1]
+                part[0].next = right
+                left = part[0]
+                temp = right
+        return ret.next
+
+so = Solution()
+show_list(so.reverseKGroup(got_list([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), 8))
