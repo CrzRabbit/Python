a59bed4a3680156688b7303b5533978b8070e1c8
wangjiangchuan <1016864609@qq.com>
2021-04-06 20:09:46 +0800

    add 0044 0049

--- leetcode/0010_isMatch_XXX.py
+++ leetcode/0010_isMatch_XXX.py
@@ -1,31 +1,30 @@
-import re
+from leetcode.tools.tool import *
 class Solution:
-    def isMatch_(self, s: str, p: str) -> bool:
-        return re.match('^{}$'.format(p), s) != None
-
-    def isMatch(self, s: str, p: str) -> bool:
+    def _isMatch(self, s: str, p: str) -> bool:
         ret = [[False for i in range(len(p) + 1)] for i in range(len(s) + 1)]
         ret[0][0] = True
+        def match(s, p, i, j):
+            if i == 0:
+                return False
+            if p[j - 1] == '.':
+                return True
+            return s[i - 1] == p[j - 1]
         for i in range(0, len(s) + 1):
             for j in range(1, len(p) + 1):
                 if p[j - 1] == '*':
-                    if self.match(s, p, i, j - 1):
+                    if match(s, p, i, j - 1):
                         ret[i][j] = ret[i - 1][j] | ret[i][j - 1] | ret[i - 1][j - 2]
                     else:
                         ret[i][j] = ret[i][j - 2]
                 else:
-                    if self.match(s, p, i, j):
+                    if match(s, p, i, j):
                         ret[i][j] = ret[i - 1][j - 1]
-
+        printMatrix(ret, s, p)
         return ret[len(s)][len(p)]
 
-    def match(self, s, p, i, j):
-        if i == 0:
-            return False
-        if p[j - 1] == '.':
-            return True
-        return s[i - 1] == p[j - 1]
-
+    def isMatch(self, s: str, p: str) -> bool:
+        pass
 
 so = Solution()
-print(so.isMatch('aasdfasdfasdfasdfas', 'aasdf.*asdf.*asdf.*asdf.*s'))
+print(so._isMatch('aasdf', 'aas*df'))
+print(so.isMatch('aasdf', 'aas*df'))
--- leetcode/tools/tool.py
+++ leetcode/tools/tool.py
@@ -6,5 +6,22 @@ def showNotCompleted():
         if re.search(r'_XXX\.py', item):
             print(item)
 
+def printMatrix(ret, s, p):
+    line = ' ' * 8
+    for j in range(len(p)):
+        line += '{:^6s}'.format(p[j])
+    print(line)
+    for i in range(0, len(s) + 1):
+        line = '  '
+        if i > 0 and i < len(s) + 1:
+            line = s[i - 1] + ' '
+        for j in range(0, len(p) + 1):
+            if ret[i][j]:
+                line += 'True  '
+            else:
+                line += 'False '
+        line += '\r\n'
+        print(line)
+
 if __name__ == '__main__':
     showNotCompleted()
