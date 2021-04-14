cafb62986df8cfec88c93486d806803e5befe5d8
CrazyRabbit <1016864609@qq.com>
2021-04-14 21:37:24 +0800

    1

--- leetcode/0208_实现 Trie (前缀树).py
+++ leetcode/0208_实现 Trie (前缀树).py
@@ -33,23 +33,82 @@ class Trie:
         """
         Initialize your data structure here.
         """
+        self.children = [None] * 26
+        self.isEnd = False
 
     def insert(self, word: str) -> None:
         """
         Inserts a word into the trie.
         """
+        node = self
+        for ch in word:
+            ch = ord(ch) - ord('a')
+            if not node.children[ch]:
+                node.children[ch] = Trie()
+            node = node.children[ch]
+        node.isEnd = True
 
+    def searchPrefix(self, word: str):
+        node = self
+        for ch in word:
+            ch = ord(ch) - ord('a')
+            if not node.children[ch]:
+                return None
+            node = node.children[ch]
+        return node
 
     def search(self, word: str) -> bool:
         """
         Returns if the word is in the trie.
         """
-
+        node = self.searchPrefix(word)
+        return node is not None and node.isEnd
 
     def startsWith(self, prefix: str) -> bool:
         """
         Returns if there is any word in the trie that starts with the given prefix.
         """
+        return self.searchPrefix(prefix) is not None
+
+class _Trie:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.children = {}
+        self.isEnd = False
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+        node = self
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch] = Trie()
+            node = node.children[ch]
+        node.isEnd = True
+
+    def searchPrefix(self, word: str):
+        node = self
+        for ch in word:
+            if ch not in node.children:
+                return None
+            node = node.children[ch]
+        return node
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+        node = self.searchPrefix(word)
+        return node is not None and node.isEnd
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+        return self.searchPrefix(prefix) is not None
 
 tr = Trie()
 print(tr.startsWith("a"))
