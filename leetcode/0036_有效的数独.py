ea90707c9b057cf4d49800bf4869cfa897603bd9
wangjiangchuan <1016864609@qq.com>
2021-04-16 20:08:49 +0800

    add 36 87

--- leetcode/0036_有效的数独.py
+++ leetcode/0036_有效的数独.py
@@ -11,6 +11,42 @@
 只需要根据以上规则，验证已经填入的数字是否有效即可。
 '''
 class Solution:
+    '''
+    依次遍历，使用字典数组保存每行，每列，每一片中出现的数字，超过2返回False
+    '''
     def isValidSudoku(self, board) -> bool:
-        pass
-        
+        rows = [{} for i in range(9)]
+        columns = [{} for i in range(9)]
+        pieces = [{} for i in range(9)]
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                t = board[i][j]
+                if t != '.':
+                    if t not in rows[i]:
+                        rows[i][t] = 1
+                    else:
+                        return False
+                    if t not in columns[j]:
+                        columns[j][t] = 1
+                    else:
+                        return False
+                    piecesIndex = (i // 3) * 3 + j // 3
+                    if t not in pieces[piecesIndex]:
+                        pieces[piecesIndex][t] = 1
+                    else:
+                        return False
+        #print(rows, columns, pieces)
+        return True
+board = \
+     [["5", "3", ".", ".", "7", ".", ".", ".", "."]
+    , ["6", ".", ".", "1", "9", "5", ".", ".", "."]
+    , [".", "9", "8", ".", ".", ".", ".", "6", "."]
+    , ["8", ".", ".", ".", "6", ".", ".", ".", "3"]
+    , ["4", ".", ".", "8", ".", "3", ".", ".", "1"]
+    , ["7", ".", ".", ".", "2", ".", ".", ".", "6"]
+    , [".", "6", ".", ".", ".", ".", "2", "8", "."]
+    , [".", ".", ".", "4", "1", "9", ".", ".", "5"]
+    , [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
+
+so = Solution()
+print(so.isValidSudoku(board))
