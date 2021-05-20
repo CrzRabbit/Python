f10e6ebaf50f72d321e1723df0dad2fb33f0fb12
wangjiangchuan <1016864609@qq.com>
2021-05-20 19:54:18 +0800

    1

--- project_infos.py
+++ project_infos.py
@@ -7,7 +7,6 @@ file_count = 0
 def get_lines(current_path):
     global line_count
     global file_count
-    #print(current_path)
     for members in os.listdir(current_path):
         if members == '__pycache__' or members == './.idea' or members == './.git':
             continue
@@ -16,11 +15,6 @@ def get_lines(current_path):
             continue
         if os.path.isfile(temp_path):
             try:
-                file = open(temp_path)
-                lines = file.readlines()
-                line_count += lines.__len__()
-                file.close()
-                print(temp_path + ': {}'.format(lines.__len__()))
                 file_count += 1
             except UnicodeDecodeError as e:
                 pass
@@ -32,16 +26,9 @@ if __name__ == '__main__':
         temp_path = current_path + members
         if os.path.isfile(temp_path):
             try:
-                file = open(temp_path)
-                lines = file.readlines()
-                line_count += lines.__len__()
-                file.close()
-                print(temp_path + ': {}'.format(lines.__len__()))
                 file_count += 1
             except UnicodeDecodeError as e:
                 pass
         else:
             get_lines(temp_path)
-    print('')
     print('total file: {}'.format(file_count))
-    print('total line: {}'.format(line_count))
