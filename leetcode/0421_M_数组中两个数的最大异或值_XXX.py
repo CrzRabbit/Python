e083980368bdc3c801d675653fcf8f956e4fedaf
wangjiangchuan <1016864609@qq.com>
2021-05-21 17:19:38 +0800

    1

--- leetcode/0421_M_数组中两个数的最大异或值_XXX.py
+++ leetcode/0421_M_数组中两个数的最大异或值_XXX.py
@@ -30,11 +30,60 @@
 '''
 from typing import List
 
+class Trie:
+    def __init__(self):
+        self.children = [None for i in range(2)]
+        self.isEnd = False
+
+    def insert(self, str):
+        node = self
+        for s in str:
+            index = ord(s) - ord('0')
+            if not node.children[index]:
+                node.children[index] = Trie()
+            node = node.children[index]
+        node.isEnd = True
+
+    def search(self, str):
+        node = self
+        ret = 0
+        for s in str:
+            index = ord(s) - ord('0')
+            rindex = 0 if index else 1
+            if node.children[rindex]:
+                ret = ret << 1
+                ret += 1
+                node = node.children[rindex]
+            elif node.children[index]:
+                ret = ret << 1
+                node = node.children[index]
+            else:
+                return 0
+        return ret
 
 class Solution:
+    '''
+    前缀树（Trie）
+    
+    '''
     def findMaximumXOR(self, nums: List[int]) -> int:
-        pass
+        def toBinStr(num):
+            bit = 30
+            str = ""
+            while bit >= 0:
+                str += '{}'.format(1 if num & 2**bit else 0)
+                bit -= 1
+            return str
+        trie = Trie()
+        ret = 0
+        for num in nums:
+            str = toBinStr(num)
+            temp = trie.search(str)
+            if temp > ret:
+                ret = temp
+            trie.insert(str)
+        return ret
 
-nums = [14,70,53,83,49,91,36,80,92,51,66,70]
+nums = [2,4]
 so = Solution()
 print(so.findMaximumXOR(nums))
