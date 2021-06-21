1233075e54a5d1386807a50615ec2df024bae901
wangjiangchuan <1016864609@qq.com>
2021-06-21 17:50:21 +0800

    1

---
+++ leetcode/0801_M_使序列递增的最小交换次数_DP.py
@@ -0,0 +1,70 @@
+'''
+我们有两个长度相等且不为空的整型数组 A 和 B 。
+
+我们可以交换 A[i] 和 B[i] 的元素。注意这两个元素在各自的序列中应该处于相同的位置。
+
+在交换过一些元素之后，数组 A 和 B 都应该是严格递增的（数组严格递增的条件仅为A[0] < A[1] < A[2] < ... < A[A.length - 1]）。
+
+给定数组 A 和 B ，请返回使得两个数组均保持严格递增状态的最小交换次数。假设给定的输入总是有效的。
+
+示例:
+输入: A = [1,3,5,4], B = [1,2,3,7]
+输出: 1
+解释:
+交换 A[3] 和 B[3] 后，两个数组如下:
+A = [1, 3, 5, 7] ， B = [1, 2, 3, 4]
+两个数组均为严格递增的。
+
+注意:
+A, B 两个数组的长度总是相等的，且长度的范围为 [1, 1000]。
+A[i], B[i] 均为 [0, 2000]区间内的整数。
+'''
+from typing import List
+
+from leetcode.tools.time import printTime
+
+
+class Solution:
+    '''
+    DP
+    '''
+    @printTime()
+    def minSwap(self, nums1: List[int], nums2: List[int]) -> int:
+        self.len = len(nums1)
+        '''
+        nex表示第i组不交换的情况下0 ~ i保持递增总共需要的最小交换次数，i = 0，nex = 0
+        ex表示第i组交换的情况下0 ~ i保持递增总共需要的最小交换次数，i = 0，ex = 1
+        1. 如果nums1[i] > nums1[i - 1], nums1[i] > nums2[i - 1]
+        nums2[i] > nums2[i - 1], nums2[i] > nums1[i - 1]
+        那么第i组交不交换都可以: ex(i) = min(ex(i - 1), nex(i - 1)) + 1
+        nex(i) = min(ex(i - 1), nex(i - 1))
+        2. 如果只满足nums1[i] > nums1[i - 1], nums1[i] > nums2[i - 1]
+        不满足nums2[i] > nums2[i - 1], nums2[i] > nums1[i - 1]
+        那么第i - 1交换，第i也得交换: ex(i) = ex(i - 1) + 1
+        第i - 1不交换，第i也不能交换: nex(i) = nex(i - 1)
+        3. 都不满足的情况下
+        那么第i - 1交换，第i就不能交换: ex(i) = nex(i - 1) + 1
+        第i - 1不交换，第i必须交换: nex(i) = ex(i - 1)
+        '''
+        nex = 0
+        ex = 1
+        for i in range(1, self.len):
+            cond1 = nums1[i] > nums1[i - 1] and nums2[i] > nums2[i - 1]
+            cond2 = nums1[i] > nums2[i - 1] and nums2[i] > nums1[i - 1]
+            t1 = nex
+            t2 = ex
+            if cond1 and cond2:
+                nex = min(t1, t2)
+                ex = min(t1, t2) + 1
+            elif cond1:
+                ex = t2 + 1
+            else:
+                nex = t2
+                ex = t1 + 1
+        return min(ex, nex)
+
+A = [0,4,4,5,9]
+B = [0,1,6,8,10]
+Solution().minSwap(A, B)
+# 0 0 1 1 1
+# 1 1 1 2 2
---
+++ leetcode/0871_H_最低加油次数_DP.py
@@ -0,0 +1,91 @@
+'''
+汽车从起点出发驶向目的地，该目的地位于出发位置东面 target 英里处。
+
+沿途有加油站，每个 station[i] 代表一个加油站，它位于出发位置东面 station[i][0] 英里处，并且有 station[i][1] 升汽油。
+
+假设汽车油箱的容量是无限的，其中最初有 startFuel 升燃料。它每行驶 1 英里就会用掉 1 升汽油。
+
+当汽车到达加油站时，它可能停下来加油，将所有汽油从加油站转移到汽车中。
+
+为了到达目的地，汽车所必要的最低加油次数是多少？如果无法到达目的地，则返回 -1 。
+
+注意：如果汽车到达加油站时剩余燃料为 0，它仍然可以在那里加油。如果汽车到达目的地时剩余燃料为 0，仍然认为它已经到达目的地。
+
+示例 1：
+输入：target = 1, startFuel = 1, stations = []
+输出：0
+解释：我们可以在不加油的情况下到达目的地。
+
+示例 2：
+输入：target = 100, startFuel = 1, stations = [[10,100]]
+输出：-1
+解释：我们无法抵达目的地，甚至无法到达第一个加油站。
+
+示例 3：
+输入：target = 100, startFuel = 10, stations = [[10,60],[20,30],[30,30],[60,40]]
+[[0,10],[10,60],[20,30],[30,30],[60,40]]
+输出：2
+解释：
+我们出发时有 10 升燃料。
+我们开车来到距起点 10 英里处的加油站，消耗 10 升燃料。将汽油从 0 升加到 60 升。
+然后，我们从 10 英里处的加油站开到 60 英里处的加油站（消耗 50 升燃料），
+并将汽油从 10 升加到 50 升。然后我们开车抵达目的地。
+我们沿途在1两个加油站停靠，所以返回 2 。
+
+提示：
+1 <= target, startFuel, stations[i][1] <= 10^9
+0 <= stations.length <= 500
+0 < stations[0][0] < stations[1][0] < ... < stations[stations.length-1][0] < target
+'''
+from typing import List
+
+from leetcode.tools.time import printTime
+
+
+class Solution:
+    '''
+    递归
+    '''
+    @printTime()
+    def minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
+        self.len = len(stations)
+        self.ret = float('inf')
+        def re(tar, sta, index, times):
+            if sta >= tar:
+                if times < self.ret:
+                    self.ret = times
+                return
+            if index >= self.len or times >= self.len:
+                return
+            dis = stations[index][0] - stations[index - 1][0] if index > 0 else stations[index][0]
+            if dis > sta:
+                return
+            re(tar - dis, sta - dis + stations[index][1], index + 1, times + 1)
+            re(tar - dis, sta - dis, index + 1, times)
+        re(target, startFuel, 0, 0)
+        return int(self.ret) if self.ret != float('inf') else -1
+
+    '''
+    DP
+    '''
+    @printTime()
+    def _1minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
+        stations = [[float('inf'), startFuel]] + stations
+        self.len = len(stations)
+        dp = [-1 for _ in range(self.len)]
+        for i in range(self.len - 1, -1, -1):
+            tar = target - stations[i][0]
+            sta = stations[i][1]
+            if sta > tar:
+                dp[i] = 0
+                continue
+            for j in range(i + 1, self.len):
+                if stations[i][0] + sta >= stations[j][0]:
+                    dp[i] = min(dp[i], dp[j] + 1)
+        return dp[0] if dp[0] != float('inf') else -1
+
+target = 1000000000
+startFuel = 145267354
+stations = [[32131797,142290934],[86397166,44642653],[99237057,56978680],[130019011,99649968],[154227249,90514223],[198652959,102942413],[272491487,108474929],[282220105,83721209],[302284128,43151624],[318501736,107636032],[359336452,73807027],[425903682,43078334],[447483572,53751173],[469840976,57311636],[472584505,57629539],[531147470,106487691],[611722638,111485731],[650472592,20105771],[692670691,141572192],[747847056,7972504],[800899205,106134661],[825649709,136473550],[880534339,72135820],[887048383,73776979],[967172408,58930710]]
+Solution().minRefuelStops(target, startFuel, stations)
+Solution()._1minRefuelStops(target, startFuel, stations)
