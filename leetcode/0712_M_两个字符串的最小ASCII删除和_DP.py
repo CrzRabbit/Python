556bd395cb1813eab8850dec229c08f17ff31d4e
CrazyRabbit <1016864609@qq.com>
2021-07-05 22:44:23 +0800

    1

---
+++ leetcode/0712_M_两个字符串的最小ASCII删除和_DP.py
@@ -0,0 +1,64 @@
+'''
+给定两个字符串s1, s2，找到使两个字符串相等所需删除字符的ASCII值的最小和。
+
+示例 1:
+
+输入: s1 = "sea", s2 = "eat"
+输出: 231
+解释: 在 "sea" 中删除 "s" 并将 "s" 的值(115)加入总和。
+在 "eat" 中删除 "t" 并将 116 加入总和。
+结束时，两个字符串相等，115 + 116 = 231 就是符合条件的最小和。
+
+示例 2:
+
+输入: s1 = "delete", s2 = "leet"
+输出: 403
+解释: 在 "delete" 中删除 "dee" 字符串变成 "let"，
+将 100[d]+101[e]+101[e] 加入总和。在 "leet" 中删除 "e" 将 101[e] 加入总和。
+结束时，两个字符串都等于 "let"，结果即为 100+101+101+101 = 403 。
+如果改为将两个字符串转换为 "lee" 或 "eet"，我们会得到 433 或 417 的结果，比答案更大。
+
+注意:
+
+0 < s1.length, s2.length <= 1000。
+所有字符串中的字符ASCII值在[97, 122]之间。
+'''
+from leetcode.tools.time import printTime
+
+
+class Solution:
+    @printTime()
+    def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        self.len1 = len(s1)
+        self.len2 = len(s2)
+        dp = [[0 for _ in range(self.len2)] for _ in range(self.len1)]
+        found1 = False
+        found2 = False
+        if s1[0] == s2[0]:
+            dp[0][0] = 0
+            found1 = True
+            found2 = True
+        else:
+            dp[0][0] = ord(s1[0]) + ord(s2[0])
+        for i in range(1, self.len2):
+            if s1[0] == s2[i] and not found1:
+                found1 = True
+                dp[0][i] = dp[0][i - 1] - ord(s2[i])
+            else:
+                dp[0][i] = dp[0][i - 1] + ord(s2[i])
+        for i in range(1, self.len1):
+            if s1[i] == s2[0] and not found2:
+                found2 = True
+                dp[i][0] = dp[i - 1][0] - ord(s1[i])
+            else:
+                dp[i][0] += dp[i - 1][0] + ord(s1[i])
+        for i in range(1, self.len1):
+            for j in range(1, self.len2):
+                dp[i][j] = min(dp[i][j - 1] + ord(s2[j]), dp[i - 1][j] + ord(s1[i]))
+                if s1[i] == s2[j]:
+                    dp[i][j] = min(dp[i][j], dp[i - 1][j - 1])
+        return dp[-1][-1]
+
+s1 = "sea"
+s2 = "eat"
+Solution().minimumDeleteSum(s1, s2)
---
+++ leetcode/0718_M_最长重复子数组_DP.py
@@ -0,0 +1,62 @@
+'''
+给两个整数数组 A 和 B ，返回两个数组中公共的、长度最长的子数组的长度。
+
+示例：
+
+输入：
+A: [1,2,3,2,1]
+B: [3,2,1,4,7]
+输出：3
+解释：
+长度最长的公共子数组是 [3, 2, 1] 。
+
+提示：
+
+1 <= len(A), len(B) <= 1000
+0 <= A[i], B[i] < 100
+'''
+from typing import List
+
+from leetcode.tools.time import printTime
+
+
+class Solution:
+    @printTime()
+    def findLength(self, nums1: List[int], nums2: List[int]) -> int:
+        self.len1 = len(nums1)
+        self.len2 = len(nums2)
+        ret = 0
+        dp = [[0 for _ in range(self.len2)] for _ in range(self.len1)]
+        if nums1[0] == nums2[0]:
+            dp[0][0] = 1
+        else:
+            dp[0][0] = 0
+        if dp[0][0] > ret:
+            ret = dp[0][0]
+        for i in range(1, self.len2):
+            if nums1[0] == nums2[i]:
+                dp[0][i] = 1
+            else:
+                dp[0][i] = 0
+            if dp[0][i] > ret:
+                ret = dp[0][i]
+        for i in range(1, self.len1):
+            if nums1[i] == nums2[0]:
+                dp[i][0] = 1
+            else:
+                dp[i][0] = 0
+            if dp[i][0] > ret:
+                ret = dp[i][0]
+        for i in range(1, self.len1):
+            for j in range(1, self.len2):
+                if nums1[i] == nums2[j]:
+                    dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + 1)
+                else:
+                    dp[i][j] = 0
+                if dp[i][j] > ret:
+                    ret = dp[i][j]
+        return ret
+
+nums1 = [1,0,0,0,1]
+nums2 = [1,0,0,1,1]
+Solution().findLength(nums1, nums2)
