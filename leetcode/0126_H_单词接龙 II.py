b728f5eaa6f3a0f81ecbdf002cda41ec39be4149
wangjiangchuan <1016864609@qq.com>
2021-08-06 17:51:12 +0800

    1

--- leetcode/0126_H_单词接龙 II.py
+++ leetcode/0126_H_单词接龙 II.py
@@ -33,6 +33,7 @@ beginWord、endWord 和 wordList[i] 由小写英文字母组成
 beginWord != endWord
 wordList 中的所有单词 互不相同
 '''
+import queue
 from typing import List
 
 from leetcode.tools.time import printTime
@@ -41,16 +42,9 @@ from leetcode.tools.time import printTime
 class Solution:
     @printTime()
     def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
-        if endWord not in wordList:
-            return []
         ret = []
-        wordList = [beginWord] + wordList
+        wordList = [beginWord] + wordList + [endWord]
         n = len(wordList)
-        for i in range(n):
-            if wordList[i] == endWord and i != 1:
-                temp = wordList[i]
-                wordList[i] = wordList[1]
-                wordList[1] = temp
         mem = {}
         def same(str1, str2):
             if len(str1) != len(str2):
@@ -71,67 +65,29 @@ class Solution:
                         mem[j] = []
                     mem[i].append(j)
                     mem[j].append(i)
-        if 1 not in mem:
-            return []
-
+        q = queue.Queue()
+        q.put([0, [0]])
+        while not q.empty():
+            index, route = q.get()
+            if wordList[index] == endWord:
+                if len(ret) == 0 or len(route) < len(ret[0]):
+                    ret.clear()
+                    ret.append([wordList[i] for i in route])
+                elif len(route) == len(ret[0]):
+                    ret.append([wordList[i] for i in route])
+                else:
+                    break
+            for i in mem[index]:
+                if i not in route:
+                    t = route.copy()
+                    t.append(i)
+                    q.put([i, t])
         return ret
-    '''
-    非解题，并查集练习
-    '''
-    @printTime()
-    def _1findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
-        if endWord not in wordList:
-            return []
-        wordList = [beginWord] + wordList
-        n = len(wordList)
-        for i in range(n):
-            if wordList[i] == endWord and i != 1:
-                temp = wordList[i]
-                wordList[i] = wordList[1]
-                wordList[1] = temp
-        fa = [i for i in range(n)]
-        rank = [1 for _ in range(n)]
-        def same(str1, str2):
-            if len(str1) != len(str2):
-                return False
-            count = 0
-            for i in range(len(str1)):
-                if str1[i] != str2[i]:
-                    count += 1
-                if count > 1:
-                    return False
-            return True
-        def find(index):
-            if fa[index] == index:
-                return index
-            fa[index] = find(fa[index])
-            return fa[index]
-        def merge(i, j):
-            fai = find(i)
-            faj = find(j)
-            if rank[i] >= rank[j]:
-                fa[faj] = fai
-            else:
-                fa[fai] = faj
-            if rank[i] == rank[j] and i != j:
-                rank[i] += 1
-        for i in range(n):
-            if i != 0 and same(wordList[0], wordList[i]):
-                merge(0, i)
-            if i != 1 and same(wordList[1], wordList[i]):
-                merge(1, i)
-        for i in range(n):
-            for j in range(i + 1, n):
-                if same(wordList[i], wordList[j]):
-                    merge(i, j)
-        print(find(0), find(1))
-        return []
 
 beginWord = "cet"
 endWord = "ism"
 wordList = ["kid","tag","pup","ail","tun","woo","erg","luz","brr","gay","sip","kay","per","val","mes","ohs","now","boa","cet","pal","bar","die","war","hay","eco","pub","lob","rue","fry","lit","rex","jan","cot","bid","ali","pay","col","gum","ger","row","won","dan","rum","fad","tut","sag","yip","sui","ark","has","zip","fez","own","ump","dis","ads","max","jaw","out","btu","ana","gap","cry","led","abe","box","ore","pig","fie","toy","fat","cal","lie","noh","sew","ono","tam","flu","mgm","ply","awe","pry","tit","tie","yet","too","tax","jim","san","pan","map","ski","ova","wed","non","wac","nut","why","bye","lye","oct","old","fin","feb","chi","sap","owl","log","tod","dot","bow","fob","for","joe","ivy","fan","age","fax","hip","jib","mel","hus","sob","ifs","tab","ara","dab","jag","jar","arm","lot","tom","sax","tex","yum","pei","wen","wry","ire","irk","far","mew","wit","doe","gas","rte","ian","pot","ask","wag","hag","amy","nag","ron","soy","gin","don","tug","fay","vic","boo","nam","ave","buy","sop","but","orb","fen","paw","his","sub","bob","yea","oft","inn","rod","yam","pew","web","hod","hun","gyp","wei","wis","rob","gad","pie","mon","dog","bib","rub","ere","dig","era","cat","fox","bee","mod","day","apr","vie","nev","jam","pam","new","aye","ani","and","ibm","yap","can","pyx","tar","kin","fog","hum","pip","cup","dye","lyx","jog","nun","par","wan","fey","bus","oak","bad","ats","set","qom","vat","eat","pus","rev","axe","ion","six","ila","lao","mom","mas","pro","few","opt","poe","art","ash","oar","cap","lop","may","shy","rid","bat","sum","rim","fee","bmw","sky","maj","hue","thy","ava","rap","den","fla","auk","cox","ibo","hey","saw","vim","sec","ltd","you","its","tat","dew","eva","tog","ram","let","see","zit","maw","nix","ate","gig","rep","owe","ind","hog","eve","sam","zoo","any","dow","cod","bed","vet","ham","sis","hex","via","fir","nod","mao","aug","mum","hoe","bah","hal","keg","hew","zed","tow","gog","ass","dem","who","bet","gos","son","ear","spy","kit","boy","due","sen","oaf","mix","hep","fur","ada","bin","nil","mia","ewe","hit","fix","sad","rib","eye","hop","haw","wax","mid","tad","ken","wad","rye","pap","bog","gut","ito","woe","our","ado","sin","mad","ray","hon","roy","dip","hen","iva","lug","asp","hui","yak","bay","poi","yep","bun","try","lad","elm","nat","wyo","gym","dug","toe","dee","wig","sly","rip","geo","cog","pas","zen","odd","nan","lay","pod","fit","hem","joy","bum","rio","yon","dec","leg","put","sue","dim","pet","yaw","nub","bit","bur","sid","sun","oil","red","doc","moe","caw","eel","dix","cub","end","gem","off","yew","hug","pop","tub","sgt","lid","pun","ton","sol","din","yup","jab","pea","bug","gag","mil","jig","hub","low","did","tin","get","gte","sox","lei","mig","fig","lon","use","ban","flo","nov","jut","bag","mir","sty","lap","two","ins","con","ant","net","tux","ode","stu","mug","cad","nap","gun","fop","tot","sow","sal","sic","ted","wot","del","imp","cob","way","ann","tan","mci","job","wet","ism","err","him","all","pad","hah","hie","aim","ike","jed","ego","mac","baa","min","com","ill","was","cab","ago","ina","big","ilk","gal","tap","duh","ola","ran","lab","top","gob","hot","ora","tia","kip","han","met","hut","she","sac","fed","goo","tee","ell","not","act","gil","rut","ala","ape","rig","cid","god","duo","lin","aid","gel","awl","lag","elf","liz","ref","aha","fib","oho","tho","her","nor","ace","adz","fun","ned","coo","win","tao","coy","van","man","pit","guy","foe","hid","mai","sup","jay","hob","mow","jot","are","pol","arc","lax","aft","alb","len","air","pug","pox","vow","got","meg","zoe","amp","ale","bud","gee","pin","dun","pat","ten","mob"]
 Solution().findLadders(beginWord, endWord, wordList)
-Solution()._1findLadders(beginWord, endWord, wordList)
 '''
 [["cet","get","gee","gte","ate","ats","its","ito","ibo","ibm","ism"],
  ["cet","cat","can","ian","inn","ins","its","ito","ibo","ibm","ism"],
---
+++ leetcode/0127_H_单词接龙.py
@@ -0,0 +1,89 @@
+'''
+字典 wordList 中从单词 beginWord 和 endWord 的 转换序列 是一个按下述规格形成的序列：
+
+序列中第一个单词是 beginWord 。
+序列中最后一个单词是 endWord 。
+每次转换只能改变一个字母。
+转换过程中的中间单词必须是字典 wordList 中的单词。
+给你两个单词 beginWord 和 endWord 和一个字典 wordList ，找到从 beginWord 到 endWord 的 最短转换序列 中的 单词数目 。如果不存在这样的转换序列，返回 0。
+
+
+示例 1：
+
+输入：beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+输出：5
+解释：一个最短转换序列是 "hit" -> "hot" -> "dot" -> "dog" -> "cog", 返回它的长度 5。
+
+示例 2：
+
+输入：beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
+输出：0
+解释：endWord "cog" 不在字典中，所以无法进行转换。
+
+
+提示：
+
+1 <= beginWord.length <= 10
+endWord.length == beginWord.length
+1 <= wordList.length <= 5000
+wordList[i].length == beginWord.length
+beginWord、endWord 和 wordList[i] 由小写英文字母组成
+beginWord != endWord
+wordList 中的所有字符串 互不相同
+'''
+import queue
+from typing import List
+
+from leetcode.tools.time import printTime
+
+
+class Solution:
+    @printTime()
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord not in wordList:
+            return 0
+        ret = 0
+        wordList = [beginWord] + wordList + [endWord]
+        n = len(wordList)
+        mem = {}
+        def same(str1, str2):
+            if len(str1) != len(str2):
+                return False
+            count = 0
+            for i in range(len(str1)):
+                if str1[i] != str2[i]:
+                    count += 1
+                if count > 1:
+                    return False
+            return True
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                if same(wordList[i], wordList[j]):
+                    if i not in mem:
+                        mem[i] = []
+                    if j not in mem:
+                        mem[j] = []
+                    mem[i].append(j)
+                    mem[j].append(i)
+        q = queue.Queue()
+        visited = [False for i in range(n)]
+        q.put([0, [0]])
+        while not q.empty():
+            index, route = q.get()
+            if wordList[index] == endWord:
+                if ret == 0 or len(route) < ret:
+                    ret = len(route)
+                else:
+                    break
+            for i in mem[index]:
+                if i not in route and visited[i] == False:
+                    t = route.copy()
+                    t.append(i)
+                    q.put([i, t])
+                    visited[i] = True
+        return ret
+
+beginWord = "hit"
+endWord = "cog"
+wordList = ["hot","dot","dog","lot","log"]
+Solution().ladderLength(beginWord, endWord, wordList)
